e35beb39d9b83861f19d186fe60e338f69436cfb
tcoppex <tcoppex@protonmail.com>
2021-05-20 20:47:34 +0200

    Add constant PiOverTwo.

--- src/shaders/shared/inc_constants.glsl
+++ src/shaders/shared/inc_constants.glsl
@@ -5,6 +5,7 @@
 
 float Epsilon()         { return 1e-6f; }
 float Pi()              { return 3.141564f; }
+float PiOverTwo()       { return 1.57079632679; }
 float TwoPi()           { return 6.283185f; }
 float GoldenAngle()     { return 2.399963f; }
 
