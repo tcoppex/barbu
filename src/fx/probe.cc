ccda291200dec4f74d91eca03e928d8df8fe8f02
tcoppex <tcoppex@protonmail.com>
2021-05-21 18:37:25 +0200

    Complete basic environment probe support.

--- src/fx/probe.cc
+++ src/fx/probe.cc
@@ -1,9 +1,24 @@
 #include "fx/probe.h"
 
+#include "memory/assets/assets.h"
+
 // ----------------------------------------------------------------------------
 
+static constexpr char const* kDefaultProbeName{ "ProbeHDR" };
+static constexpr uint32_t kProbeInternalFormat{ GL_RGBA16F }; //
+
+// ----------------------------------------------------------------------------
+
+// CubeFace iterration array.
+EnumArray<CubeFace, CubeFace> const Probe::kIterFaces{
+  CubeFace::PosX, CubeFace::NegX, 
+  CubeFace::PosY, CubeFace::NegY, 
+  CubeFace::PosZ, CubeFace::NegZ
+};
+
+// Probe::ViewController face view matrices.
 // [this could be reduced to 3x3 matrices]
-const std::array<glm::mat4, 6> Probe::kViewMatrices{
+EnumArray<glm::mat4, CubeFace> const Probe::ViewController::kViewMatrices{
   glm::lookAt(glm::vec3(0.0f), glm::vec3( 1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
   glm::lookAt(glm::vec3(0.0f), glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)),
   glm::lookAt(glm::vec3(0.0f), glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec3(0.0f,  0.0f,  1.0f)),
@@ -12,7 +27,82 @@ const std::array<glm::mat4, 6> Probe::kViewMatrices{
   glm::lookAt(glm::vec3(0.0f), glm::vec3( 0.0f,  0.0f, -1.0f), glm::vec3(0.0f, -1.0f,  0.0f))
 };
 
+// Shared probe camera [does not handle concurrency].
+Camera Probe::sCamera;
+
 // ----------------------------------------------------------------------------
 
+void Probe::init(int32_t const resolution) {
+  resolution_ = resolution;
+
+  // Initialize the shared camera if needed.
+  if (sCamera.fov() <= 0.0f) {
+    sCamera.set_perspective(90.0f, 1, 1, 0.1f, 500.0f); //
+  }
+
+  glCreateFramebuffers( 1, &fbo_);
+
+  // Attach a renderbuffer for depth testing.
+  glCreateRenderbuffers( 1, &renderbuffer_);
+  glNamedRenderbufferStorage( renderbuffer_, GL_DEPTH_COMPONENT24, resolution_, resolution_);
+  glNamedFramebufferRenderbuffer( fbo_, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, renderbuffer_);
+
+  // Create a HDR cubemap texture.
+  texture_ = TEXTURE_ASSETS.createCubemap( 
+    kDefaultProbeName, 1, kProbeInternalFormat, resolution_, resolution_
+  );
+  assert( texture_ != nullptr );
+  glNamedFramebufferTexture(fbo_, GL_COLOR_ATTACHMENT0, texture_->id, 0);
+  
+  if (!gx::CheckFramebufferStatus()) {
+    LOG_ERROR( "Probe framebuffer completion has failed." );
+  }
+
+  CHECK_GX_ERROR();
+}
+
+void Probe::release() {
+  if (fbo_ != 0u) {
+    glDeleteFramebuffers( 1, &fbo_);
+    fbo_ = 0u;
+
+    glDeleteRenderbuffers( 1, &renderbuffer_);
+    renderbuffer_ = 0u;
+  }
+
+  // [todo : release texture asset]
+}
+
+void Probe::begin() {
+  sCamera.set_controller(&view_controller_);
+  gx::Viewport(resolution_, resolution_);
+  glBindFramebuffer(GL_FRAMEBUFFER, fbo_);
+}
+
+void Probe::end() {
+  sCamera.set_controller(nullptr);
+  glBindFramebuffer(GL_FRAMEBUFFER, 0u);
+}
+
+Camera const& Probe::setup_face(CubeFace face) {
+  auto const target{ GL_TEXTURE_CUBE_MAP_POSITIVE_X + static_cast<int32_t>(face) };
+  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, target, texture_->id, 0);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); //
+  
+  // Update camera internal matrices.
+  view_controller_.set_face(face);
+  sCamera.rebuild();
+
+  return sCamera;
+}
+
+void Probe::process(DrawCallback_t draw_cb) {
+  begin();
+  for (auto const& face : kIterFaces) {
+    setup_face(face);
+    draw_cb(sCamera);
+  }
+  end();
+}
 
 // ----------------------------------------------------------------------------
--- src/fx/probe.h
+++ src/fx/probe.h
@@ -1,11 +1,26 @@
 #ifndef BARBU_FX_PROBE_H_
 #define BARBU_FX_PROBE_H_
 
-#include <array>
+#include <functional>
 #include "glm/glm.hpp"
 
+#include "core/camera.h"
+#include "memory/enum_array.h"
 #include "memory/assets/texture.h"
-#include "memory/assets/program.h"
+
+// ----------------------------------------------------------------------------
+
+// Enum to face indices of a cube map.
+enum class CubeFace {
+  PosX = 0,
+  NegX,
+  PosY,
+  NegY,
+  PosZ,
+  NegZ,
+
+  kCount
+};
 
 // ----------------------------------------------------------------------------
 
@@ -14,16 +29,67 @@
 //
 class Probe {
  public:
-  // View matrices for each faces of an axis aligned cube.
-  static const std::array<glm::mat4, 6> kViewMatrices;
+  static constexpr int32_t kDefaultCubemapResolution = 512;
+
+  // Array to iter over cubeface enums.
+  static EnumArray<CubeFace, CubeFace> const kIterFaces;
 
  public:
-  Probe() = default;
+  using DrawCallback_t = std::function<void(Camera const&)>;
+
+  Probe()
+    : resolution_(0u)
+    , fbo_(0u)
+    , renderbuffer_(0u)
+    , texture_(nullptr)
+  {}
+
+  ~Probe() {
+    release();
+  }
+
+  void init(int32_t const resolution = kDefaultCubemapResolution);
+  void release();
+
+  void begin();
+  void end();
+
+  // Prepare the face to render and return its view matrices.
+  Camera const& setup_face(CubeFace face);
+
+  // Update all 6 faces of the cubemap by providing a draw callback expecting 
+  // a view projection matrix. 
+  void process(DrawCallback_t draw_cb);
+
+  inline int32_t resolution() const { return resolution_; }
+  inline TextureHandle texture() const { return texture_; }
 
  private:
-  ProgramHandle program_;
-  TextureHandle cubemap_;
+  // Probe camera view controller.
+  class ViewController final : public Camera::ViewController {
+   public:
+    // View matrices for each faces of an axis aligned cube.
+    static EnumArray<glm::mat4, CubeFace> const kViewMatrices;
+
+    virtual ~ViewController() {}
+
+    inline void set_face(CubeFace face) { face_ = face; }
+    inline void get_view_matrix(float *m) final { memcpy(m, glm::value_ptr(kViewMatrices[face_]), 16 * sizeof(float)); } 
+    inline glm::vec3 target() const final { return glm::vec3(0.0f); }
+
+   private:
+    CubeFace face_ = CubeFace::PosX;
+  } view_controller_;
+  
+  // Shared probe camera. 
+  static Camera sCamera;
+
   int32_t resolution_;
+
+  // [todo : use generic objects]
+  uint32_t fbo_;
+  uint32_t renderbuffer_;
+  TextureHandle texture_;
 };
 
 // ----------------------------------------------------------------------------
