8601b373fcb30a090079313759a3e52119720667
tcoppex <tcoppex@protonmail.com>
2021-05-24 13:59:03 +0200

    Add ordering operator for vec{2, 3}, add custom map for vec{2, 3, 4}.

--- src/utils/mathutils.h
+++ src/utils/mathutils.h
@@ -2,6 +2,7 @@
 #define BARBU_UTILS_MATHUTILS_H_
 
 #include <cmath>
+#include <map>
 #include "glm/glm.hpp"
 
 // ----------------------------------------------------------------------------
@@ -17,20 +18,63 @@ constexpr bool almost_equal(T const& a, T const& b, T const eps = std::numeric_l
       || (distance < (std::numeric_limits<T>::min() * glm::abs(b + a)));
 }
 
-// Order comparison between two vec3.
+// ----------------------------------------------------------------------------
+
 template<typename T>
-struct vec3_ordering {
+struct vec2_ordering {
   static
   bool Less(const T& a, const T& b) {
     return (a.x < b.x)
-        || (almost_equal(a.x, b.x) && (a.y < b.y))
-        || (almost_equal(a.x, b.x) && almost_equal(a.y, b.y) && (a.z < b.z));
+        || (almost_equal(a.x, b.x) && (a.y < b.y));
   }
 
   bool operator() (const T& a, const T& b) const {
     return Less(a, b);
   }
 };
+
+template<typename T>
+struct vec3_ordering {
+  static
+  bool Less(const T& a, const T& b) {
+    const bool testX = almost_equal(a.x, b.x);
+    return (a.x < b.x)
+        || (testX && (a.y < b.y))
+        || (testX && almost_equal(a.y, b.y) && (a.z < b.z));
+  }
+
+  bool operator() (const T& a, const T& b) const {
+    return Less(a, b);
+  }
+};
+
+template<typename T>
+struct vec4_ordering {
+  static
+  bool Less(const T& a, const T& b) {
+    const bool testX = almost_equal(a.x, b.x);
+    const bool testY = almost_equal(a.y, b.y);
+    return (a.x < b.x)
+        || (testX && (a.y < b.y))
+        || (testX && testY && (a.z < b.z))
+        || (testX && testY && almost_equal(a.z, b.z) && (a.w < b.w));
+  }
+
+  bool operator() (const T& a, const T& b) const {
+    return Less(a, b);
+  }
+};
+
+// ----------------------------------------------------------------------------
+
+template<typename K, typename V>
+using MapVec2 = std::map<K, V, vec2_ordering<K>>;
+
+template<typename K, typename V>
+using MapVec3 = std::map<K, V, vec3_ordering<K>>;
+
+template<typename K, typename V>
+using MapVec4 = std::map<K, V, vec4_ordering<K>>;
 
 } // namespace
 
