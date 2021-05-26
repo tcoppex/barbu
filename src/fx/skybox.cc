bb2c7da68528d0cc758a67bc500d6eefcafdf849
tcoppex <tcoppex@protonmail.com>
2021-05-26 22:00:33 +0200

    Skybox : add support for prefiltering convolution & cleanup initialization.

--- src/fx/skybox.cc
+++ src/fx/skybox.cc
@@ -9,10 +9,11 @@
 
 // Used to debug visualize the convolution pass output.
 static bool constexpr kVisualizeIrradianceMap = false;
+static bool constexpr kVisualizeSpecularMap   = false;
 
 // ----------------------------------------------------------------------------
 
-void Skybox::init() {
+void Skybox::init(ResourceId resource_id) {
   // Compute program to transform a spherical map to a cubemap.
   pgm_.cs_transform = PROGRAM_ASSETS.createCompute( 
     SHADERS_DIR "/skybox/cs_spherical_to_cubemap.glsl" 
@@ -33,11 +34,18 @@ void Skybox::init() {
     SHADERS_DIR "/skybox/fs_convolution.glsl"
   );
 
+  //
+  pgm_.prefilter = PROGRAM_ASSETS.createRender(
+    "skybox::prefilter",
+    SHADERS_DIR "/skybox/vs_skybox.glsl",
+    SHADERS_DIR "/skybox/fs_prefiltering.glsl"
+  );
+
   // [ use a generic one ]
   // Skybox mesh.
   cube_mesh_ = MESH_ASSETS.createCube();
 
-  setup_texture(); //
+  setup_texture(resource_id); //
 
   CHECK_GX_ERROR();
 }
@@ -45,71 +53,47 @@ void Skybox::init() {
 void Skybox::deinit() {
   cube_mesh_.reset(); //
   sky_map_.reset(); //
+  irradiance_map_.reset();
+  specular_map_.reset();
 }
 
 void Skybox::render(Camera const& camera) {
   render(RenderMode::Sky, camera);
 }
 
-// ----------------------------------------------------------------------------
 
-void Skybox::render(RenderMode mode, Camera const& camera) {
-  auto const pgm = (mode == RenderMode::Sky) ? pgm_.render->id : pgm_.convolution->id;
+void Skybox::setup_texture(ResourceId resource_id) {
+  assert(sky_map_ == nullptr);
 
-  // Scale the box and recenter it to the camera.
-  // [ we do not need to scale as we clamp depth in the vertex shader to assure
-  // maximum scaling, that's just show the alternative. ]
-  glm::mat4 view = camera.view();
-  view[3] = glm::vec4(glm::vec3(0.0f), view[3].w);
-  auto const mvp = camera.proj() * glm::scale( view, glm::vec3(camera.far()));
-  gx::SetUniform( pgm, "uMVP", mvp);
+  // (we might want to use mipmaps for late convolutions)
+  constexpr int32_t kLevels = 1;
 
-  auto const tex_id = (kVisualizeIrradianceMap && irradiance_map_) ? irradiance_map_->id : sky_map_->id;
+  auto const kSkyboxCubemapID = TEXTURE_ASSETS.findUniqueID( "Skybox::Cubemap" );
+  auto const basename   = Resource::TrimFilename(resource_id);
+  bool const is_crossed = basename.find("cross") != std::string::npos;
 
-  int32_t texture_unit = -1;
-  gx::BindTexture( tex_id, ++texture_unit, gx::LinearClamp);
-  gx::SetUniform( pgm, "uCubemap", texture_unit);
+  // Environment sky map.
+  if (is_crossed) {
+    // -- Crossed HDR map --
 
-  gx::UseProgram(pgm);
-    cube_mesh_->draw();
-  gx::UseProgram(0u);
-
-  gx::UnbindTexture();
-  
-  CHECK_GX_ERROR();
-}
-
-// ----------------------------------------------------------------------------
-
-void Skybox::setup_texture(/*ResourceInfo info*/) {
-  // Environment map.
-  if constexpr (true) {
-    // Crossed HDR map.
-
-    ResourceInfo resource_info( 
-      ASSETS_DIR "/textures/cross_hdr/uffizi_cross_mmp_s.hdr"
-    );
-    sky_map_ = TEXTURE_ASSETS.createCubemapHDR( resource_info.id );
+    sky_map_ = TEXTURE_ASSETS.createCubemapHDR( kSkyboxCubemapID, kLevels, resource_id);
 
     if (sky_map_ && sky_map_->loaded()) {
-      Irradiance::PrefilterHDR( resource_info, sh_matrices_);
+      Irradiance::PrefilterHDR( ResourceInfo(resource_id), sh_matrices_);
       has_sh_matrices_ = true;
     }
   } else {
-    // Spherical HDR.
+    // -- Spherical HDR --
 
     constexpr int32_t kResolution = Probe::kDefaultCubemapResolution; //
-    constexpr int32_t kNumFaces = Probe::kViewMatrices.size(); //
-    constexpr uint32_t kFormat = GL_RGBA16F; //
+    constexpr int32_t kNumFaces   = Probe::kViewMatrices.size(); //
+    constexpr uint32_t kFormat    = GL_RGBA16F; //
 
-    // Input spherical texture.
-    auto spherical_tex = TEXTURE_ASSETS.create2d( 
-      ASSETS_DIR "/textures/reinforced_concrete_02_1k.hdr",
-      1, kFormat 
-    ); //
+    // Input spherical texture [tmp].
+    auto spherical_tex = TEXTURE_ASSETS.create2d( resource_id, 1, kFormat); //
     
     // Output sky cubemap.
-    sky_map_ = TEXTURE_ASSETS.createCubemap( "skybox::Cubemap", 1, kFormat, kResolution, kResolution );
+    sky_map_ = TEXTURE_ASSETS.createCubemap( kSkyboxCubemapID, kLevels, kFormat, kResolution, kResolution ); //
 
     // Transform spherical map to cubical.
     {
@@ -130,35 +114,94 @@ void Skybox::setup_texture(/*ResourceInfo info*/) {
         gx::DispatchCompute<16, 16>( kResolution, kResolution, kNumFaces);
       gx::UseProgram(0u);
 
-      glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+      glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT); //
+
+      // Generate sub mips levels when needed.
+      if (kLevels > 1) {
+        sky_map_->generate_mipmaps();
+      }
 
       // Unbind all.
       gx::UnbindTexture( texture_unit );
-      glBindImageTexture( image_unit, 0, 0, GL_FALSE, 0, GL_WRITE_ONLY, kFormat);
+      glBindImageTexture( image_unit, 0, 0, GL_FALSE, 0, GL_WRITE_ONLY, kFormat); //
     }
   }
 
-  // Create the irradiance envmap when needed.
+  // Prove use to generate the envmap convolutions.
+  Probe probe;
+
+  // Be sure to have set the proper pipeline states (Already done in the renderer).
+  // gx::Enable( gx::State::CubeMapSeamless );
+  // gx::Disable( gx::State::CullFace );
+
+  // Irradiance envmap.
   if (!has_sh_matrices_) {
     constexpr int32_t kIrradianceMapResolution = 64;
-    
-    // ---
-    // gx::Enable( gx::State::CubeMapSeamless );
-    // gx::Disable( gx::State::CullFace );
-    // ---
-
-    Probe probe;
-    probe.init(kIrradianceMapResolution, false);
-    probe.capture( [this](Camera const& camera) {
+    probe.setup( kIrradianceMapResolution, 1, false);
+    probe.capture( [this](Camera const& camera, int32_t level) {
       render( RenderMode::Convolution, camera); 
     });
-
     irradiance_map_ = probe.texture();
-
-    LOG_DEBUG_INFO( "Skybox irradiance map has been generated (instead of SH matrices)." );
-  } else {
-    LOG_DEBUG_INFO( "Skybox SH matrices has been generated (instead of irradiance map)." );
   }
+  LOG_DEBUG_INFO( "Skybox map", basename, "use",  has_sh_matrices_ ? "SH matrices." : "an irradiance map." );
+
+  // --------------------------------------
+  
+  // [Work in Progress]
+  // Specular envmap.
+  if constexpr(true) {
+    constexpr int32_t kSpecularMapNumSamples = 2048; //
+    constexpr int32_t kSpecularMapResolution = 256; //
+    constexpr int32_t kSpecularMapLevel      = Texture::GetMaxMipLevel(kSpecularMapResolution);
+    constexpr float kInvMaxLevel             = 1.0f / (kSpecularMapLevel - 1.0f);
+
+    pgm_.prefilter->setUniform( "uNumSamples", kSpecularMapNumSamples);
+    probe.setup( kSpecularMapResolution, kSpecularMapLevel, false); //
+    probe.capture( [this, kInvMaxLevel](Camera const& camera, int32_t level) {
+      const float roughness = level * kInvMaxLevel;
+      pgm_.prefilter->setUniform( "uRoughness",  roughness); //
+      render( RenderMode::Prefilter, camera); 
+    });
+    specular_map_ = probe.texture();
+  }
+}
+
+
+void Skybox::render(RenderMode mode, Camera const& camera) {
+  auto const pgm = (mode == RenderMode::Sky)         ? pgm_.render->id : 
+                   (mode == RenderMode::Convolution) ? pgm_.convolution->id
+                                                     : pgm_.prefilter->id
+                                                     ;
+
+  // Scale the box and recenter it to the camera.
+  // [ we do not need to scale as we clamp depth in the vertex shader to assure
+  // maximum scaling, that's just show the alternative. ]
+  glm::mat4 view = camera.view();
+  view[3] = glm::vec4(glm::vec3(0.0f), view[3].w);
+  auto const mvp = camera.proj() * glm::scale( view, glm::vec3(camera.far()));
+  gx::SetUniform( pgm, "uMVP", mvp);
+
+  uint32_t tex_id = sky_map_->id;
+  
+  // Debug visualization.
+  if (mode == RenderMode::Sky) {
+    if (kVisualizeIrradianceMap && irradiance_map_) {
+      tex_id = irradiance_map_->id;
+    } else if (kVisualizeSpecularMap && specular_map_) {
+      tex_id = specular_map_->id;
+    }
+  }
+
+  constexpr int32_t texture_unit = 0;
+  gx::BindTexture( tex_id, texture_unit, gx::LinearMipmapClamp);
+  gx::SetUniform( pgm, "uCubemap", texture_unit);
+
+  gx::UseProgram(pgm);
+    cube_mesh_->draw();
+  gx::UseProgram(0u);
+
+  gx::UnbindTexture();
+  CHECK_GX_ERROR();
 }
 
 // ----------------------------------------------------------------------------
--- src/fx/skybox.h
+++ src/fx/skybox.h
@@ -15,44 +15,48 @@ class Camera;
 //
 // Manages HDR environment maps and their irradiance contribution.
 //
-// Irradiance contributions are stored in both spherical harmonics
-// matrices and as cubemap. 
+// * Irradiance contributions are stored either as spherical harmonics matrices 
+//   or as cubemap. 
 //
 class Skybox {
  public:
   Skybox() = default;
 
-  void init();
+  void init(ResourceId resource_id);
   void deinit();
+
+  void setup_texture(ResourceId resource_id); //
 
   void render(Camera const& camera);
 
   TextureHandle texture() { return sky_map_; }
   TextureHandle irradiance_map() { return irradiance_map_; }
+  TextureHandle specular_map() { return specular_map_; } //
 
   inline glm::mat4 const* irradiance_matrices() const { return sh_matrices_.data(); }
 
   inline bool has_irradiance_matrice() const { return has_sh_matrices_; }
 
  private:
-  void setup_texture(/*ResourceInfo info*/);
-
   enum class RenderMode {
     Sky,
     Convolution,
+    Prefilter, //
   };
   void render(RenderMode mode, Camera const& camera);
 
   struct {
     ProgramHandle cs_transform;             //< transform spherical map to cube map.  
     ProgramHandle render;                   //< render skybox.
-    ProgramHandle convolution;              //< convolute irradiance map.  
+    ProgramHandle convolution;              //< convolute irradiance map.
+    ProgramHandle prefilter;  //            //< prefilter specular map.
   } pgm_;
 
   MeshHandle cube_mesh_; //
 
   TextureHandle sky_map_;                   //< sky cubemap.
   TextureHandle irradiance_map_;            //< irradiance envmap.
+  TextureHandle specular_map_; //           //< 
 
   Irradiance::SHMatrices_t sh_matrices_;    //< irradiances spherical harmonics matrices.
   bool has_sh_matrices_ = false;
