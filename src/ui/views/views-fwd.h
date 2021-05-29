e9c74613848a8db7ab059310be5c3b7308c7c172
tcoppex <tcoppex@protonmail.com>
2021-05-29 09:27:17 +0200

    Add UI views forward declarations.

---
+++ src/ui/views/views-fwd.h
@@ -0,0 +1,19 @@
+#ifndef BARBU_UI_VIEWS_VIEWS_FWD_H_
+#define BARBU_UI_VIEWS_VIEWS_FWD_H_
+
+namespace views {
+
+class Main; //
+class RenderView;
+
+class TexturesView;
+
+class HairView;
+class MarschnerView;
+
+class SceneHierarchyView;
+
+}
+
+#endif  // BARBU_UI_VIEWS_VIEWS_FWD_H_
+
