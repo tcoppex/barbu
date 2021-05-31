545024b1edc2dd5ad5a3fe4e63bcad9f80f1e090
tcoppex <tcoppex@protonmail.com>
2021-05-31 23:53:41 +0200

    Add implementation file for material.

---
+++ src/ecs/material.cc
@@ -0,0 +1,68 @@
+#include "ecs/material.h"
+
+#include "glm/glm.hpp"
+#include "core/graphics.h"
+#include "core/logger.h"
+
+// ----------------------------------------------------------------------------
+
+int32_t Material::update_uniforms(RenderAttributes const& attributes, int32_t default_unit) {
+  texture_unit_ = default_unit; //
+
+  bool const use_new_program{ texture_unit_ == 0 };
+  
+  if (use_new_program) {
+    auto pgm_handle = program();
+    if (!pgm_handle) {
+      LOG_FATAL_ERROR( "Material program \"", program_id_.c_str(), "\" not found." );
+    }
+
+    auto const pgm = pgm_handle->id;
+    gx::UseProgram( pgm );
+
+    auto bind_texture = [this, &pgm](auto const& name, uint32_t id, gx::SamplerName samplerName = gx::SamplerName::kDefaultSampler) {
+      if (id > 0u) {
+        gx::BindTexture( id, texture_unit_, samplerName);
+        gx::SetUniform( pgm, name, texture_unit_);
+        ++texture_unit_;  
+      }
+    };
+
+    // (vertex)
+    gx::SetUniform( pgm, "uMVP",         attributes.mvp_matrix);
+    gx::SetUniform( pgm, "uModelMatrix", attributes.world_matrix);
+
+    // (vertex skinning)
+    if (attributes.skinning_texid > 0u) {
+      bind_texture( "uSkinningDatas",   attributes.skinning_texid,  gx::SamplerName::LinearClamp);
+      
+      // [ improve ]
+      skinning_subroutines_[ SkinningMode::LinearBlending ] = glGetSubroutineIndex(pgm, GL_VERTEX_SHADER, "skinning_LBS");
+      skinning_subroutines_[ SkinningMode::DualQuaternion ] = glGetSubroutineIndex(pgm, GL_VERTEX_SHADER, "skinning_DQBS");
+      auto const& su_index = skinning_subroutines_[ attributes.skinning_mode ];
+      LOG_CHECK( su_index != GL_INVALID_INDEX );
+      glUniformSubroutinesuiv(GL_VERTEX_SHADER, 1, &su_index);
+    }
+
+    // (fragment)
+    bind_texture( "uBRDFMap",           attributes.brdf_lut_texid,   gx::SamplerName::LinearClamp);
+    bind_texture( "uPrefilterEnvmap",   attributes.prefilter_texid,  gx::SamplerName::LinearMipmapClamp);
+    bind_texture( "uIrradianceEnvmap",  attributes.irradiance_texid, gx::SamplerName::LinearClamp);
+
+    bool const has_irradiance_matrices = (nullptr != attributes.irradiance_matrices);
+    gx::SetUniform( pgm, "uHasIrradianceMatrices", has_irradiance_matrices);
+    if (has_irradiance_matrices) {
+      gx::SetUniform( pgm, "uIrradianceMatrices", attributes.irradiance_matrices, 3);
+    }
+
+    gx::SetUniform( pgm, "uEyePosWS", attributes.eye_position);
+    //gx::SetUniform(pgm, "uToneMapMode",       static_cast<int>(attributes.tonemap_mode));
+  }
+  CHECK_GX_ERROR();
+
+  update_internals();
+
+  return texture_unit_;
+}
+
+// ----------------------------------------------------------------------------
