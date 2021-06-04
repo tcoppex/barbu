d8efe5fc0a03e0e72ebb4b4decffff187816c4b1
tcoppex <tcoppex@protonmail.com>
2021-06-04 13:23:02 +0200

    Add support for attribute lighting.

--- src/shaders/unlit/fs_unlit.glsl
+++ src/shaders/unlit/fs_unlit.glsl
@@ -2,16 +2,17 @@
 
 // ----------------------------------------------------------------------------
 
-// Outputs.
+layout(location = 0) in vec3 inColor;
 layout(location = 0) out vec4 fragColor;
 
 // Uniforms.
-uniform vec4 uColor = vec4(1.0f);
+uniform vec4 uColor = vec4(0.75, 0.5, 0.5, 1.0);
+uniform bool uUseAttribColor = false;
 
 // ----------------------------------------------------------------------------
 
 void main() {
-  fragColor = uColor;
+  fragColor = uUseAttribColor ? vec4(inColor, 1.0) : uColor;
 }
 
 // ----------------------------------------------------------------------------
--- src/shaders/unlit/vs_unlit.glsl
+++ src/shaders/unlit/vs_unlit.glsl
@@ -4,6 +4,9 @@
 
 // Inputs.
 layout(location = 0) in vec3 inPosition;
+layout(location = 1) in vec3 inAttrib;
+
+layout(location = 0) out vec3 outColor;
 
 // Uniforms.
 uniform mat4 uMVP;
@@ -12,6 +15,7 @@ uniform mat4 uMVP;
 
 void main() {
   gl_Position = uMVP * vec4(inPosition, 1.0);
+  outColor = 0.5 * inAttrib + 0.5;
 }
 
 // ----------------------------------------------------------------------------
