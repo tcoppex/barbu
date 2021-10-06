903646d2107d501fb1d6c2e3b752c84cfa9c130b
tcoppex <tcoppex@protonmail.com>
2021-10-06 11:54:43 +0200

    Revamped for the new event system.

--- src/Application.cc
+++ src/Application.cc
@@ -7,60 +7,106 @@
 
 void Application::setup() {
   // Gamma-corrected clear color.
-  gx::ClearColor(0.145f, true);
+  gx::ClearColor(0.15f, 0.12f, 0.12f, true);
 
   // Camera.
   {
-    auto const rx = glm::pi<float>() / 16.0f;
-    auto const ry = glm::pi<float>() / 8.0f;
-    arcball_controller_.set_view( rx, ry, true);
-    arcball_controller_.set_dolly(5.75f);
+    float const rx{ + glm::pi<float>() / 16.0f };
+    float const ry{ + glm::pi<float>() / 8.0f };
+    arcball_controller_.set_view( rx, ry, false);
+    arcball_controller_.set_dolly(16.0f, false);
 
-    auto const& res = resolution();
     camera_.set_controller(&arcball_controller_);
-    camera_.set_perspective(glm::radians(60.0f), res.x, res.y, 0.01f, 500.0f);
+    camera_.set_perspective(glm::radians(60.0f), resolution(), 0.01f, 500.0f);
   }
 
   // Skybox texture.
   renderer_.skybox().setup_texture(
-    ASSETS_DIR "/textures/cross_hdr/uffizi_cross_mmp_s.hdr"
-    // ASSETS_DIR "/textures/reinforced_concrete_02_1k.hdr"
+    ASSETS_DIR "/textures/forest_slope_2k.hdr"
   );
-  //renderer_.params().show_skybox = false;
 
   // Scene Hierarchy.
-  if constexpr(true) {
-    if constexpr (true) {
-      // -- Hair sample --
-
-      focus_ = scene_.import_model( ASSETS_DIR "/models/InfiniteScan/Head.glb" );
-      renderer_.hair().setup( ASSETS_DIR "/models/InfiniteScan/Head_scalp.obj" ); //
-      scene_.add_bounding_sphere(0.25f);
-    } else {
-      // -- Simple model sample --
-
-      focus_ = scene_.import_model( 
-        // ASSETS_DIR "/models/gltf_samples/MetalRoughSpheres/MetalRoughSpheres.gltf"
-        // ASSETS_DIR "/models/gltf_samples/CesiumMan.glb"
-        // ASSETS_DIR "/models/gltf_samples/DamagedHelmet.glb"
-        // ASSETS_DIR "/models/glb-heads/DigitalIra.glb"
-        ASSETS_DIR "/models/sintel/sintel_blendshapes.glb" 
-      );
-    }
+  {
+    focus_ = scene_.import_model( ASSETS_DIR "/models/InfiniteScan/Head.glb" );
   }
-  bRefocus_ = true;
+  
+  // Set renderer parameters.
+  {
+    auto &params{ renderer_.params() };
+    params.show_skybox = true;
+    params.show_grid = true;
+    params.enable_hair = false;
+    params.enable_particle = false;
+  }
 
-  // Recenter the view on the focus centroid.
-  //refocus_camera( focus_, /*FocusOnCentroid*/true, /*NoTransition*/false);
+  //---------------------------
+
+  bRefocus_ = false;
 }
 
 void Application::update() {
-  auto const& eventData{ GetEventData() };
-  auto const& selected = scene_.selected();  
+  auto const& selected = scene_.selected();
 
+  // Refocus the camera when asked for.
+  if (bRefocus_) {
+    refocus_camera( /*kCentroid*/ true, /*kSmooth*/ false, focus_);
+    bRefocus_ = false;
+  } else {
+    focus_ = selected.empty() ? nullptr : 
+                       focus_ ? focus_ : selected.front();
+  }
+
+  // -----------------------
+
+  // [Bug-prone due to SceneHierarchy internal structure update, to check]
+
+  // Modify the SceneHierarchy.
+  if constexpr(true) {
+    // Notes: Adding new entities should be done separately of scene hierarchy modification
+    //        as it has not update its internal structure yet.
+
+    auto const& events = Events::Get(); //
+    
+    if (!selected.empty()) {
+      // Reset transform.
+      if ('x' == events.lastInputChar()) {
+        for (auto &e : selected) {
+          scene_.reset_entity(e);
+        }
+      } 
+      // Delete.
+      else if ('X' == events.lastInputChar()) {
+        for (auto &e : selected) {
+          scene_.remove_entity(e, true);
+          // focus_ = (focus_ == e) ? nullptr : focus_;
+        }
+        scene_.select_all(false);
+      }
+    }
+
+    // Import drag-n-dropped objects & center them to camera target.
+    //constexpr float kDragNDropDistance = 2.0f;
+    auto const dnd_target = camera_.target(); //camera.position() + kDragNDropDistance * camera.direction();
+    for (auto &fn : events.droppedFilenames()) {
+      if (auto const ext = fn.substr(fn.find_last_of(".") + 1); MeshDataManager::CheckExtension(ext)) {
+        if (auto entity = scene_.import_model(fn); entity) {
+          entity->set_position( dnd_target );
+        }
+      }
+    }
+  }
+
+  // -----------------------
+}
+
+void Application::onInputChar(uint16_t inputChar) {
+  auto const& selected = scene_.selected();
+
+  constexpr bool kCentroid{true};
+  constexpr bool kSmooth{true};
 
   // -- Key bindings.
-  switch (eventData.lastChar) {
+  switch (inputChar) {
     // Select  / Unselect all.
     case 'a':
       scene_.select_all(selected.empty());
@@ -68,26 +114,25 @@ void Application::update() {
     
     // Focus on entities.
     case 'C':
-      refocus_camera( false, false);
+      refocus_camera( !kCentroid, kSmooth);
     break;
     case 'c':
-      refocus_camera( true, false);
+      refocus_camera( kCentroid, kSmooth);
     break;
     case GLFW_KEY_1:
     case GLFW_KEY_3:
     case GLFW_KEY_7:
-      refocus_camera( true, true);
-
+      refocus_camera( kCentroid, !kSmooth);
     break;
 
     // Cycle through entities.
     case 'j':
-      refocus_camera( false, false, 
+      refocus_camera( !kCentroid, kSmooth, 
         selected.front() ? scene_.next(selected.front(), +1) : scene_.first()
       );
     break;
     case 'k':
-      refocus_camera( false, false, 
+      refocus_camera( !kCentroid, kSmooth, 
         selected.front() ? scene_.next(selected.front(), -1) : scene_.first()
       );
     break;
@@ -105,80 +150,43 @@ void Application::update() {
     default:
     break;
   } 
-
-  if (bRefocus_) {
-    refocus_camera(true, true, focus_);
-    bRefocus_ = false;
-  } else {
-    focus_ = selected.empty() ? nullptr : focus_ ? focus_ : selected.front();
-  }
-
-  // --------------------------
-
-  // Modify the SceneHierarchy.
-  if constexpr(true) {
-    // Notes: Adding new entities should be done separately of scene hierarchy modification
-    //        as it has not update its internal structure yet.
-
-    if (!selected.empty()) {
-      // Reset transform.
-      if ('x' == eventData.lastChar) {
-        for (auto &e : selected) {
-          scene_.reset_entity(e);
-        }
-      } 
-      // Delete.
-      else if ('X' == eventData.lastChar) {
-        for (auto &e : selected) {
-          scene_.remove_entity(e, true);
-          // focus_ = (focus_ == e) ? nullptr : focus_;
-        }
-        scene_.select_all(false);
-      }
-    }
-
-    // Import drag-n-dropped objects & center them to camera target.
-    //constexpr float kDragNDropDistance = 2.0f;
-    auto const dnd_target = camera_.target(); //camera.position() + kDragNDropDistance * camera.direction();
-    for (auto &fn : eventData.dragFilenames) {
-      auto const ext = fn.substr(fn.find_last_of(".") + 1);
-      if (MeshDataManager::CheckExtension(ext)) {
-        if (auto e = scene_.import_model(fn); e) {
-          e->set_position( dnd_target );
-        }
-      }
-    }
-  }
 }
 
-void Application::refocus_camera(bool bCentroid, bool bNoSmooth, EntityHandle new_focus) {
-  glm::vec3 target;
-  float radius = 3.5f;
+void Application::onResize(int w, int h) {
+  LOG_MESSAGE("Resize :", w, h);
+  gx::Viewport( w, h);
+}
+
+// ----------------------------------------------------------------------------
+
+void Application::refocus_camera(bool bCentroid, bool bSmooth, EntityHandle new_focus) {
+  constexpr float kDefaultRefocusDistance{3.5f};
+  constexpr float kRefocusDistanceScaling{1.25f};
 
   // FIXME : issue due to scene reindexing ordering !
 
   // Select new focus & retrieve target position.
+  glm::vec3 target{};
   if (new_focus && new_focus->indexed()) {
     focus_ = new_focus;
     scene_.select_all(false);
-    scene_.select(new_focus, true);
-    target = bCentroid ? scene_.entity_global_centroid(new_focus) 
-                       : scene_.entity_global_position(new_focus)
+    scene_.select(focus_, true);
+    target = bCentroid ? scene_.entity_global_centroid(focus_) 
+                       : scene_.entity_global_position(focus_)
                        ;
   } else {
     target = bCentroid ? scene_.centroid() : scene_.pivot();
   }
 
   // Set camera view controller target.
-  arcball_controller_.set_target(target, bNoSmooth);
+  arcball_controller_.set_target(target, bSmooth);
   
   // Distance to focus point.
-  if (focus_ && focus_->has<VisualComponent>()) {
-    // radius should be scale with entity global scale.
-    auto visual = focus_->get<VisualComponent>();
-    radius = visual.mesh()->radius();
-  }
-  arcball_controller_.set_dolly(1.25f * radius, bNoSmooth);
+  float const radius{ 
+    (focus_ && focus_->has<VisualComponent>()) ? focus_->get<VisualComponent>().mesh()->radius() 
+                                               : kDefaultRefocusDistance
+  };
+  arcball_controller_.set_dolly(kRefocusDistanceScaling * radius, bSmooth);
 }
 
 // ----------------------------------------------------------------------------
--- src/Application.h
+++ src/Application.h
@@ -13,13 +13,27 @@ class Application final : public App {
   ~Application() {}
 
   void setup() final;
+  
   void update() final;
+
+  /* -- Events Capture -- */
+
+  // void onKeyPressed(KeyCode_t key) final;
+  // void onKeyReleased(KeyCode_t key) final;
+  void onInputChar(uint16_t c) final;
+  // void onMousePressed(int x, int y, KeyCode_t button) final;
+  // void onMouseReleased(int x, int y, KeyCode_t button) final;
+  // void onMouseEntered(int x, int y) final;
+  // void onMouseExited(int x, int y) final;
+  // void onMouseMoved(int x, int y) final;
+  // void onMouseDragged(int x, int y, KeyCode_t button) final;
+  // void onMouseWheel(float dx, float dy) final;
+  void onResize(int w, int h) final;
 
  private:
   void refocus_camera(bool bCentroid, bool bNoSmooth, EntityHandle new_focus = nullptr);
 
   ArcBallController arcball_controller_;
-
   EntityHandle focus_;
   bool bRefocus_ = false;
 };
