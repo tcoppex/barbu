9b500212dae8335345dc0fb1a2fd5ee431ad9b11
tcoppex <tcoppex@protonmail.com>
2021-11-05 18:59:10 +0100

    Add a bounding sphere entity template.

---
+++ src/ecs/entities/bsphere.h
@@ -0,0 +1,36 @@
+#ifndef BARBU_ECS_ENTITIES_BSPHERE_H_
+#define BARBU_ECS_ENTITIES_BSPHERE_H_
+
+#include "ecs/entity.h"
+#include "ecs/components/sphere_collider.h"
+
+// ----------------------------------------------------------------------------
+
+//
+//  A prebuilt entity that store a bounding sphere collider.
+//
+class BSphereEntity : public Entity {
+ private:
+  // Default name given to bounding sphere entities.
+  static constexpr char const* kDefaultBoundingSphereEntityName{ "BSphere" };
+
+ public:
+  BSphereEntity(std::string_view _name, float _radius)
+    : Entity(_name)
+  {
+    auto &sphere = add<SphereColliderComponent>();
+    sphere.set_radius(_radius);
+  }
+
+  BSphereEntity(float _radius)
+    : BSphereEntity(kDefaultBoundingSphereEntityName, _radius)
+  {}
+
+  float radius() const {
+    return get<SphereColliderComponent>().radius();
+  }
+};
+
+// ----------------------------------------------------------------------------
+
+#endif // BARBU_ECS_ENTITIES_BSPHERE_H_
