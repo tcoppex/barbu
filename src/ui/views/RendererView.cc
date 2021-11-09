9bcf3e2ac85e0299898266bc299445222bf91717
tcoppex <tcoppex@protonmail.com>
2021-11-09 23:39:23 +0100

    [UI] Add a debug section to renderer.

--- src/ui/views/RendererView.cc
+++ src/ui/views/RendererView.cc
@@ -15,16 +15,22 @@ void RendererView::render() {
   // Rendering parameters.
   ImGui::SetNextItemOpen(true, ImGuiCond_Once);
   if (ImGui::TreeNode("General")) {
-    // ImGui::Checkbox("Show gizmo",           &params_.show_transform);
     ImGui::Checkbox("Show grid",            &params_.show_grid);
     ImGui::Checkbox("Show skybox",          &params_.show_skybox);
     ImGui::Checkbox("Show rigs",            &params_.show_rigs);
-    // ImGui::Checkbox("Show wireframe (w)",   &params_.show_wireframe);
     ImGui::Checkbox("Show hair",            &params_.enable_hair);
-    ImGui::Checkbox("Show particle",        &params_.enable_particle);
-  
+    ImGui::Checkbox("Show particles",       &params_.enable_particle);
     ImGui::TreePop();
   }
+
+#ifndef NDEBUG
+  if (ImGui::TreeNode("Debug")) {
+    ImGui::Checkbox("Post-process",         &params_.enable_postprocess);
+    // ImGui::Checkbox("Show wireframe (w)",   &params_.show_wireframe);
+    // ImGui::Checkbox("Show gizmo",           &params_.show_transform);
+    ImGui::TreePop();
+  }
+#endif
 
   ImGui::Spacing();
   ImGui::Separator();
